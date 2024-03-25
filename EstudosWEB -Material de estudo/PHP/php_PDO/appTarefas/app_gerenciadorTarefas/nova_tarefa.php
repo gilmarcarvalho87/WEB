98ceabcd1bbf53ba9a7575ba4abe1ca897d38091
Gilmar Carvalho <gilmarpcarvalho@gmail.com>
2024-03-25 19:45:08 -0300

    25-03-24

--- EstudosWEB -Material de estudo/PHP/php_PDO/appTarefas/app_gerenciadorTarefas/nova_tarefa.php
+++ EstudosWEB -Material de estudo/PHP/php_PDO/appTarefas/app_gerenciadorTarefas/nova_tarefa.php
@@ -41,13 +41,13 @@
 				</div>
 
 				<div class="col-md-9">
-					<div class="container pagina">
+					<div class="container pagina">	
 						<div class="row">
 							<div class="col">
 								<h4>Nova tarefa</h4>
 								<hr/>
 
-								<form method="POST" action="tarefaController.php">
+								<form method="POST" action="tarefaController.php?acao=inserir">
 									<div class="form-group">
 										<label>Descrição da tarefa:</label>
 										<input type="text" name="tarefa" class="form-control" placeholder="Exemplo: Lavar o carro">
--- EstudosWEB -Material de estudo/PHP/php_PDO/appTarefas/app_gerenciadorTarefasPrivado/tarefaController.php
+++ EstudosWEB -Material de estudo/PHP/php_PDO/appTarefas/app_gerenciadorTarefasPrivado/tarefaController.php
@@ -4,19 +4,26 @@ require"../../app_gerenciadorTarefasPrivado/conexao_pdo.php";
 require"../../app_gerenciadorTarefasPrivado/tarefaModel.php";
 require"../../app_gerenciadorTarefasPrivado/tarefaService.php";
 
+if(isset($_POST['acao']) && $_POST['acao'] == 'inserir') {    
+        
+            $tarefa = new Tarefa();
+        //salva o valor que vem do front e salva na variavel
+
+        $tarefa->__set('tarefa',$_POST['tarefa']);
+
+        $conexao =new Conexao();
+        //recebera retorno da conexao e a tarefa  a ser executada
+        $tarefaService =new TarefaService($conexao,$tarefa);
+
+        $tarefaService->inserir();
+        header("location:nova_tarefa.php?inclusao=1"); 
+}else{
+        
 
 
-$tarefa = new Tarefa();
-//salva o valor que vem do front e salva na variavel
+}
 
-$tarefa->__set('tarefa',$_POST['tarefa']);
 
-$conexao =new Conexao();
-//recebera retorno da conexao e a tarefa  a ser executada
-$tarefaService =new TarefaService($conexao,$tarefa);
-
-$tarefaService->inserir();
-header("location:nova_tarefa.php?inclusao=1"); 
 
     
 
