2d7af53907e9d9c5e656e136fe3f62a199737e12
Gilmar Carvalho <gilmarpcarvalho@gmail.com>
2024-04-24 23:03:05 -0300

    24-04-24

---
+++ EstudosWEB -Material de estudo/JQUERY/manipulandoElementosHtml.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <title>Manipulando Elementos internos</title>
+
+    <style>
+        .btn{
+            height:100px;
+            width:100px;
+            border: 1px solid red;
+            margin: 50px auto;
+        }
+
+    </style>
+    <script>
+        $(document).ready(() =>{
+
+            $('#btn1').html('<h2  style="color:red"> novo 1 </h2>')
+            $('#btn2').html('<h2 style="color: green"> novo 2 </h2> ')
+            $('#btn3').html('<h2 style="color:blue"> novo 3 </h2> ')
+      
+            
+    
+    
+        })
+
+
+    </script>
+
+</head>
+
+<body>
+
+    <div class="div btn" id="btn1"> botao 1</div>
+    <div class="div btn" id="btn2" value="div2"></div>
+    <div class="div btn" id="btn3" value="div3"></div>
+   
+    
+</body>
+</html>
