760a28c01535eb2b9e42f88f7126b70b2868543b
Gilmar Carvalho <gilmarpcarvalho@gmail.com>
2024-06-21 21:47:15 -0300

    iniciando estrutura padrao mvc

---
+++ EstudosWEB -Material de estudo/MVC/miniframework/App/Controllers/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Controllers\
+
+class IndexController{
+    public function index{
+        echo 'chegamos ao index controller action index';
+
+    }
+    public function sobreNos{
+        echo 'chegamos ao index controller action sobreNos';
+
+    }
+
+
+}
+
+
+
+
+
+?> 
